379644a5f01bd0bbd2c2a3a81a363defd531fe30
Dustin OBrien <dcobrien82@gmail.com>
2017-01-17 03:00:09 +0000

    function created. GITHUB username and token added.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = 'dustinob';
+var GITHUB_TOKEN = "5496f73a65ad252cc335b9c823c5cbf9db9a939c";
+
+
+
+
+function getRepoContributors (repoOwner, repoName, callback) {
+
+var requestURL = 'http://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
