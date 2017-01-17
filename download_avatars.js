21a5ca8cda7df5086be9da302c32c68bb546ba0a
Dustin OBrien <dcobrien82@gmail.com>
2017-01-17 04:33:25 +0000

    JSON object parsed in the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,30 @@
 var request = require('request');
-
+var fs = require('fs');
 
 var GITHUB_USER = 'dustinob';
 var GITHUB_TOKEN = "5496f73a65ad252cc335b9c823c5cbf9db9a939c";
 
 
-
-
 function getRepoContributors (repoOwner, repoName, callback) {
 
-var requestURL = 'http://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   console.log(requestURL);
+
+  var options = {
+    url: requestURL,
+    headers: {'user-agent': 'dustinob'}
+  }
+
+  request.get(options, function(err, response) {
+    var parsed = JSON.parse(response.body);
+    console.log(parsed);
+    //console.log(JSON.parse(response));
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+
+  });
