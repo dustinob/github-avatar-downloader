593651f0e5352a3dd20e560ced2842337d289058
Dustin OBrien <dcobrien82@gmail.com>
2017-01-17 06:02:36 +0000

    check for arguments included. Project complete.

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,14 @@
+var args = process.argv;
 var request = require('request');
 var fs = require('fs');
 
 var GITHUB_USER = 'dustinob';
 var GITHUB_TOKEN = "5496f73a65ad252cc335b9c823c5cbf9db9a939c";
 
+if(!args[2] || !args[3]) {
+    console.log("Need some arguments bro!")
+    return false;
+  }
 
 function getRepoContributors (repoOwner, repoName, callback) {
 
@@ -18,13 +23,33 @@ function getRepoContributors (repoOwner, repoName, callback) {
 
   request.get(options, function(err, response) {
     var parsed = JSON.parse(response.body);
-    console.log(parsed);
-    //console.log(JSON.parse(response));
+    parsed.forEach(function(element) {
+      if(!fs.existsSync('./avatars')) {
+        fs.mkdirSync('./avatars');
+      }
+
+      downloadImageByURL(element.avatar_url,'./avatars/' + element.login + '.jpg')
+    })
+
   });
 }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
+function downloadImageByURL (url, filePath) {
+
+ request.get(url)
+        .on('error', function (err) {
+          throw err;
+        })
+        .on('response', function (response) {
+          console.log('Response Status Code: ', response.statusCode);
+          console.log("Downloading Image...")
+        })
+        .pipe(fs.createWriteStream(filePath));
+}
+
+  getRepoContributors(args[2], args[3], function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
 
   });
+
